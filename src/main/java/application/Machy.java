5f44c66c681d09de591218c4a5803800e76d01e9
kekich119 <patowach3o@gmail.com>
2025-06-03 05:28:42 +0300

    ещё пару задачек

--- src/main/java/application/Machy.java
+++ src/main/java/application/Machy.java
@@ -1,4 +1,18 @@
 package application;
 
+import java.util.stream.Stream;
+
 public class Machy {
+    public static void main(String[] args) {
+
+    }
+
+
+    boolean hasNegativeEvenNumbers(Stream<Integer> stream){
+        if(stream.anyMatch(x -> x < 0 && x % 2 == 0)){
+            return true;
+        }else {
+            return false;
+        }
+    }
 }
--- src/main/java/application/Product.java
+++ src/main/java/application/Product.java
@@ -1,4 +1,19 @@
 package application;
 
+import java.time.LocalDate;
+import java.util.stream.Stream;
+
 public class Product {
+    private String name;
+    private LocalDate expirationDate;
+
+    public Product(String name, LocalDate expirationDate) {
+        this.name = name;
+        this.expirationDate = expirationDate;
+    }
+
+    public LocalDate getExpirationDate() {
+        return expirationDate;
+    }
+
 }
