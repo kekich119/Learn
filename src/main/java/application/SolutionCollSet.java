d3a244f69c87225406cd6fbabccae8e1d5dcb073
kekich119 <patowach3o@gmail.com>
2025-06-04 12:46:28 +0300

    Решил две задачки на преобразование из потока в List и Set

--- src/main/java/application/SolutionCollSet.java
+++ src/main/java/application/SolutionCollSet.java
@@ -1,4 +1,21 @@
 package application;
 
+import java.util.Collections;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class SolutionCollSet {
+    public static Set<String> getFilteredString(Stream<String> stream){
+        return stream
+                .filter(x -> x.length() > 6)
+                .collect(Collectors.toSet());
+    }
+
+    public static void main(String[] args) {
+        Stream<String> stream = Stream.of("z","Привет дурачок", "ZOVZ");
+        System.out.println(getFilteredString(stream));
+    }
+
+
 }
--- src/main/java/application/SoulutionCollect.java
+++ src/main/java/application/SoulutionCollect.java
@@ -1,4 +1,19 @@
 package application;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class SoulutionCollect {
+    public static List<Integer> getPositiveNumbers (Stream<Integer> stream){
+        return stream
+                .filter(x -> x > 0).collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+        Stream<Integer> stream = Stream.of(1,3,4,5);
+        System.out.println(getPositiveNumbers(stream));
+    }
 }
